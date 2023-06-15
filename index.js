a216c96e64392861b63c2bf1f9cb072165e7c935
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-06-15 17:14:51 +0300

    completed assingment

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+ return 2000 / 100
+ console.log(divide);   
+}
+const square= (parameter) =>{
+    return parameter * parameter;
+    console.log(square(5));
+}
+const add =(parameter1, parameter2) =>{
+    return parameter1 + parameter2;
+    console.log(add(3, 7));
+}
